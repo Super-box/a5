38b5455f49c11ebb84e3cd6222e2e911b3e16311
Super-box <37006424+Super-box@users.noreply.github.com>
2019-01-03 09:30:59 +0800

    Update updatessl.sh

--- updatessl.sh
+++ updatessl.sh
@@ -1,4 +1,4 @@
 if ! wget -N --no-check-certificate https://github.com/Super-box/a5/raw/master/ssl.zip -O /etc/ocserv/ssl.zip; then
 		    echo -e "${Error} ocserv 服务 配置文件下载失败 !" && exit
 fi
-            unzip -o /etc/ocserv/ssl.zip -d /etc/ocserv
+            unzip -o /etc/ocserv/ssl.zip -d /etc/ocserv/ssl
